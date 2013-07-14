5125fd400fffd17f8c251448a753fd93f9acc847
humanitiesNerd <catonano@gmail.com>
2013-07-14 10:27:22 +0200

    further cleanup. Call (-main path/to/the/file.txt)

--- src/stammtisch/core.clj
+++ src/stammtisch/core.clj
@@ -4,30 +4,6 @@
   [criterium.core :as cr])
   )
 
-
-
-(defn print-it [ ]
-  (let [to-be-printed (take 10 (process-my-file))]
-    (dorun (map #(println %) to-be-printed ))  )
-  )
-
-
-(defn main* []
-(print-it)
-)
-
-
-(defn -main [& args]
-  (case (first args)
-    ; The -b option does a benchmark. This implies head retention.
-    ; There's no other way: we cannot reprocess standard input.
-    ("-b" "--bench")
-      (cr/quick-bench (main*) :verbose)
-      (main*)
-  )
-)
-
-
 (defn lines [file-reader]
     (line-seq file-reader)
 )
@@ -36,13 +12,38 @@
   (re-seq #"[a-z]+" line)
   )
 
+
 (defn process-my-file-improved [file-reader]
  (sort-by second > (frequencies (mapcat words-per-line (lines file-reader)))
  )
  )
 
-(defn my-main []
-   (with-open [mio-file (clojure.java.io/reader "/home/catonano/Berlino/stammtisch/moby-dic-originale.txt")]
+
+;(my-main "/home/catonano/Berlino/stammtisch/moby-dic-originale.txt")
+;(-main "/home/catonano/Berlino/stammtisch/moby-dic-originale.txt")
+(defn my-main [path-to-the-file]
+   (with-open [mio-file (clojure.java.io/reader path-to-the-file)]
       (process-my-file-improved mio-file)
    ) 
+)
+
+(defn print-it [path-to-file]
+  (let [to-be-printed (take 10 (my-main path-to-file))]
+    (dorun (map #(println %) to-be-printed ))  )
+  )
+
+
+(defn main* [path-to-file]
+(print-it path-to-file)
+)
+
+
+(defn -main [path-to-file & args]
+  (case (first args)
+    ; The -b option does a benchmark. This implies head retention.
+    ; There's no other way: we cannot reprocess standard input.
+    ("-b" "--bench")
+      (cr/quick-bench (my-main path-to-file) :verbose)
+      (my-main path-to-file)
+  )
 )
