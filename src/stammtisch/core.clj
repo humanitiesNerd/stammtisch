9c38ca1aa15b07585ccae32fe7f266918e86411e
humanitiesNerd <catonano@gmail.com>
2013-07-16 10:39:18 +0200

    first step in making it multiversion. For now the steps all do the old vanilla thing

--- src/stammtisch/core.clj
+++ src/stammtisch/core.clj
@@ -1,9 +1,16 @@
 (ns stammtisch.core
   (:gen-class)
- 
   (:require clojure.java.io
   [criterium.core :as cr])
   )
+
+
+
+;(my-main "/home/catonano/Berlino/stammtisch/moby-dic-originale.txt")
+;(-main "/home/catonano/Berlino/stammtisch/moby-dic-originale.txt")
+; (-new-main "/home/catonano/Berlino/stammtisch/moby-dic-originale.txt" "vanilla")
+;(-new-main "/home/catonano/Berlino/stammtisch/moby-dic-originale.txt" "reducers")
+;(-new-main "/home/catonano/Berlino/stammtisch/moby-dic-originale.txt" "meikel")
 
 (defn lines-in-the-file [file-reader]
    (line-seq file-reader)
@@ -24,24 +31,63 @@
 )
 
 
-;(my-main "/home/catonano/Berlino/stammtisch/moby-dic-originale.txt")
-;(-main "/home/catonano/Berlino/stammtisch/moby-dic-originale.txt")
-(defn my-main [path-to-the-file]
+
+
+(defn vanilla [path-to-the-file]
    (with-open [mio-file (clojure.java.io/reader path-to-the-file)]
       (process-my-file-improved mio-file)
-   ) 
+   )
 )
 
-(defn print-it [path-to-file]
-  (let [to-be-printed (take 10 (my-main path-to-file))]
+(defn meikel [path-to-the-file]
+   (with-open [mio-file (clojure.java.io/reader path-to-the-file)]
+      (process-my-file-improved mio-file)
+   )
+)
+
+(defn reducers [path-to-the-file]
+   (with-open [mio-file (clojure.java.io/reader path-to-the-file)]
+      (process-my-file-improved mio-file)
+   )
+)
+
+
+(defn print-it-vanilla [path-to-file]
+  (let [to-be-printed (take 10 (vanilla path-to-file))]
+    (dorun (map #(println %) to-be-printed ))  )
+  )
+
+(defn print-it-meikel [path-to-file]
+  (let [to-be-printed (take 10 (meikel path-to-file))]
+    (dorun (map #(println %) to-be-printed ))  )
+  )
+
+(defn print-it-reducers [path-to-file]
+  (let [to-be-printed (take 10 (meikel path-to-file))]
     (dorun (map #(println %) to-be-printed ))  )
   )
 
 
+(defn print-it [path-to-file]
+  (let [to-be-printed (take 10 (vanilla path-to-file))]
+    (dorun (map #(println %) to-be-printed ))  )
+  )
+
+(defn main-vanilla* [path-to-file]
+(print-it-vanilla path-to-file)
+)
+
+(defn main-meikel* [path-to-file]
+(print-it-meikel path-to-file)
+)
+
+(defn main-reducers* [path-to-file]
+(print-it-reducers path-to-file)
+)
+
 (defn main* [path-to-file]
 (print-it path-to-file)
 )
-
 
 (defn -main [path-to-file & args]
   (case (first args)
@@ -51,4 +97,28 @@
       (cr/quick-bench (main* path-to-file) :verbose)
       (main* path-to-file)
   )
+)
+
+(defn -new-main [path-to-file version & args]
+   (let [benchmarking (first args)]
+   (case version
+      ("vanilla")
+         (if (= benchmarking "-b")
+            (cr/quick-bench (main-vanilla* path-to-file) :verbose)
+            (main-vanilla* path-to-file)
+         )
+      ("reducers")
+         (if (= benchmarking "-b")
+            (cr/quick-bench (main-reducers* path-to-file) :verbose)
+            (main-reducers* path-to-file)
+         )
+
+      ("meikel")
+         (if (= benchmarking "-b")
+            (cr/quick-bench (main-meikel* path-to-file) :verbose)
+            (main-meikel* path-to-file)
+         )
+
+   )
+   )
 )
