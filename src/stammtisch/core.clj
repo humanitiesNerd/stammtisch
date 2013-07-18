7972c29708d6ef698e0341dc6ed2fed6f04fb447
humanitiesNerd <catonano@gmail.com>
2013-07-18 15:56:41 +0200

    dead code elimination

--- src/stammtisch/core.clj
+++ src/stammtisch/core.clj
@@ -21,83 +21,32 @@
 )
 
 
-(defn vanilla-process-my-file [file-reader]
- (sort-by second > (frequencies (vanilla-words (lines-in-the-file file-reader)))
+(defn vanilla-process-my-file [path-to-the-file]
+ (with-open [mio-file (clojure.java.io/reader path-to-the-file)]
+   (sort-by second > (frequencies (vanilla-words (lines-in-the-file mio-file)))
+   )
  )
 )
 
-
-
-
-(defn vanilla [path-to-the-file]
-   (with-open [mio-file (clojure.java.io/reader path-to-the-file)]
-      (vanilla-process-my-file mio-file)
-   )
+(defn set-up [version path-to-the-file]
+  (case version
+  ("vanilla")
+     (vanilla-process-my-file path-to-the-file)
+  ("meikel")
+     (meikel-process-my-file path-to-the-file)   
+  )
 )
 
-(defn meikel [path-to-the-file]
-      ;(process-my-file-improved mio-file)
-      (meikel-process-my-file path-to-the-file)
-)
-
-(defn reducers [path-to-the-file]
-   (with-open [mio-file (clojure.java.io/reader path-to-the-file)]
-      (vanilla-process-my-file mio-file)
-   )
-)
-
-
-(defn print-it-vanilla [path-to-file]
-  (let [to-be-printed (take 10 (vanilla path-to-file))]
+(defn print-it [version path-to-file]
+  (let [to-be-printed (take 10 (set-up version path-to-file))]
     (dorun (map #(println %) to-be-printed ))  )
   )
-
-(defn print-it-meikel [path-to-file]
-  (let [to-be-printed (take 10 (meikel path-to-file))]
-    (dorun (map #(println %) to-be-printed ))  )
-  )
-
-(defn print-it-reducers [path-to-file]
-  (let [to-be-printed (take 10 (meikel path-to-file))]
-    (dorun (map #(println %) to-be-printed ))  )
-  )
-
-
-(defn main-vanilla* [path-to-file]
-(print-it-vanilla path-to-file)
-)
-
-(defn main-meikel* [path-to-file]
-(print-it-meikel path-to-file)
-)
-
-(defn main-reducers* [path-to-file]
-(print-it-reducers path-to-file)
-)
-
-
-
 
 (defn -main [path-to-file version & args]
    (let [benchmarking (first args)]
-   (case version
-      ("vanilla")
          (if (= benchmarking "-b")
-            (cr/quick-bench (main-vanilla* path-to-file) :verbose)
-            (main-vanilla* path-to-file)
+            (cr/quick-bench (print-it version path-to-file) :verbose)
+            (print-it version path-to-file)
          )
-      ("reducers")
-         (if (= benchmarking "-b")
-            (cr/quick-bench (main-reducers* path-to-file) :verbose)
-            (main-reducers* path-to-file)
-         )
-
-      ("meikel")
-         (if (= benchmarking "-b")
-            (cr/quick-bench (main-meikel* path-to-file) :verbose)
-            (main-meikel* path-to-file)
-         )
-
-   )
    )
 )
